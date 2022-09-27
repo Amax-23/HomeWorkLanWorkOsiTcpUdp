5a657e3493619a9642da92cea95db07c59a2feb9
Amax-23 <maksim.agoshkov@gmail.com>
2022-09-27 13:05:29 +0300

    created a client and a server according to the task conditions

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -7,18 +7,15 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
-    private static final int PORT = 8080;
+    private static final int PORT = 8989;
     private static final String HOST = "127.0.0.1";
 
     public static void main(String[] args) {
         try (Socket clientSocet = new Socket(HOST, PORT);
              PrintWriter out = new PrintWriter(clientSocet.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocet.getInputStream()));) {
-            out.println("Клиент");
-            out.println("Соединение установленно клиентом с порта сервера " + clientSocet.getPort());
             System.out.println(in.readLine());
-            System.out.println(in.readLine());
-            System.out.println(in.readLine());
+            out.println("воронеж");
             System.out.println(in.readLine());
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -8,26 +8,37 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    private static final int PORT = 8080;
+    private static final int PORT = 8989;
 
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {// стартуем сервер один(!) раз
             System.out.println("Server started!");
+            System.out.println("Waiting connection on server port:" + PORT);
+            String city = "";
             while (true) {
                 try (Socket clientSocet = serverSocket.accept();
                      PrintWriter out = new PrintWriter(clientSocet.getOutputStream(), true);
                      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocet.getInputStream()));) {
-                    System.out.println("New connection accepted!");
-                    final String name = in.readLine();
-                    out.println(String.format("Hi %s. your port is %d", name, clientSocet.getPort()));
-                    out.println("Вы установили соединение с адреса " + clientSocet.getInetAddress());
-                    out.println("Вы установили соединение с порта " + clientSocet.getPort());
-                    out.println("Вы установили соединение на запушенном сервере с портом " + clientSocet.getLocalPort());
-                    System.out.println(in.readLine());
+                    if (city.length() == 0) {
+                        out.println("???");
+                        city = in.readLine();
+                        out.println("OK");
+                        System.out.println(city);
+                    } else {
+                        out.println("Последний введенный город - " + city + ", Вам на - " + city.charAt(city.length() - 1));
+                        String city2 = in.readLine();
+                        if (city.charAt(city.length() - 1) == city2.charAt(0)) {
+                            out.println("OK!");
+                            city = city2;
+                        } else {
+                            out.println("NOT OK!");
+                        }
+                    }
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
         }
     }
 }
